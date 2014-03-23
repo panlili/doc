60012d075be6707ec54ba070a50b0e33d70ffbf5
flysword <flyswordr@2013-0504-1456>
2014-03-23 15:52:40 +0800

    base thinkphp

--- Conf/config.php
+++ Conf/config.php
@@ -1,20 +1,5 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-     'URL_CASE_INSENSITIVE' => true,
-    'LOAD_EXT_FILE' => 'tpl',
-    'LAYOUT_ON' => true,
-    'DEFAULT_MODULE' => 'User',
-    'DEFAULT_ACTION' => 'index',
-    'DEFAULT_CHARSET' => 'utf-8',
-    'DB_TYPE' => 'mysql',
-    'DB_HOST' => 'localhost',
-    'DB_NAME' => 'doc',
-    'DB_USER' => 'root',
-    'DB_PWD' => '57419',
-    //'DB_PWD' => 'cuitcuit',
-    'DB_PORT' => '3306',
-    //'DB_PREFIX' => 'sjf_',
-    
 );
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
-//加载框架入口文件
-    require './ThinkPHP/ThinkPHP.php';
+
+define("THINK_PATH", "./ThinkPHP/");
+define("APP_PATH", "./");
+define("APP_NAME", "aiyouxi");
+define("APP_DEBUG", true);
+
+require(THINK_PATH . "ThinkPHP.php");
 ?>
