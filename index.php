5b154d2be3196cc34b464f85446f58e8f971fc95
flysword <flyswordr@2013-0504-1456>
2014-04-06 00:55:12 +0800

    数据库重新清空，基本完成

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 define("THINK_PATH", "./ThinkPHP/");
 define("APP_PATH", "./");
 define("APP_NAME", "aiyouxi");
-define("APP_DEBUG", true);
+define("APP_DEBUG", false);
 
 require(THINK_PATH . "ThinkPHP.php");
 ?>
